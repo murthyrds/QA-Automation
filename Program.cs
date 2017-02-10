5bb5d031bf1c20afb66aab15db8c5efe79bcd524
murthyrds <murthygl.rds@gmail.com>
2017-02-10 12:41:26 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿
+using System.Threading.Tasks;
+using OpenQA.Selenium.Chrome;
+
+
+namespace qaApplication
+{
+    public class Program
+    {
+        const string CARJOJO_URL = "https://fe.carjojo.com";
+
+        public static void Main(string[] args)
+        {
+            // download and unzip driver in project folder http://chromedriver.storage.googleapis.com/index.html?path=2.27/
+            var driver = new ChromeDriver(".");
+            try
+            {
+                driver.Navigate().GoToUrl(CARJOJO_URL);
+                driver.FindElementByClassName("brand").Click();
+                Task.Delay(1000).Wait();
+                driver.FindElementByClassName("model").Click();
+                // Task.Delay(2000).Wait();
+                // driver.FindElementByXPath("//*button[@class='button expand carjojo-button strange-limey-yellow']").Click();
+                // Task.Delay(1000).Wait();
+                // driver.FindElementByXPath("//*[contains(text(), 'SHOW VEHICLES')]").Click();
+            }
+            catch(System.Exception ex)
+            {
+                System.Console.WriteLine(ex.ToString());
+                System.Console.WriteLine("Closing browser in 3 sec ...");
+            }
+            finally
+            {
+                // Close the browser after 3 sec
+                Task.Delay(3000).Wait();
+                driver.Quit();
+            }           
+        }
+    }
+}
